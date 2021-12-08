183724a5b4e97e98f67404384567d72ad1747301
相撞相拥 <9762205+mishuo123@user.noreply.gitee.com>
2021-12-08 20:05:57 +0800

    初始化项目结构

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div></div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+
+  //注册组件
+  components: {},
+
+  //生命周期 - 创建完成
+  created() {},
+
+  //DOM挂载完毕
+  mounted() {},
+};
+</script>
+
+<style lang='less' scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {};
+  },
+  methods: {},
+
+  //注册组件
+  components: {},
+
+  //生命周期 - 创建完成
+  created() {},
+
+  //DOM挂载完毕
+  mounted() {},
+};
 </script>
+
+<style lang='less' scoped>
+</style>
